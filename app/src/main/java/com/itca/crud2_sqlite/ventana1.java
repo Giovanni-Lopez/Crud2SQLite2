82a0907076a04d67afa29dbe5c856d1903a7e778
jamt29 <82690615+Giovanni-Lopez@users.noreply.github.com>
2021-09-16 11:50:09 -0600

    se creo el archivo ventana.xml y se añadieron iconos etc

---
+++ app/src/main/java/com/itca/crud2_sqlite/ventana1.java
@@ -0,0 +1,14 @@
+package com.itca.crud2_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ventana1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ventana1);
+    }
+}
